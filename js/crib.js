353a703ff7d5227683391b2f49a461c3e1d06b2a
Zachary McGee <zachary.mcgee@siu.edu>
2019-02-17 00:41:01 -0600

    Update crib.js

---
+++ js/crib.js
@@ -0,0 +1,126 @@
+function cribdrag() {
+  //var cribword = ascii_to_hex(document.getElementById("cribword").value);
+  console.log(xor_hex("3b101c091d53320c000910", "071d154502010a04000419"));
+  //console.log(cribword);
+}
+
+function setup() {
+  updateXORTable();
+  updateCribTable();
+}
+
+setup();
+
+document.getElementById("ciphertext1").addEventListener('input', function (evt) {
+  updateXORTable();
+});
+
+document.getElementById("ciphertext2").addEventListener('input', function (evt) {
+  updateXORTable();
+});
+
+document.getElementById("cribword").addEventListener('input', function (evt) {
+  updateCribTable();
+});
+
+function hex_to_ascii(str) {
+    var hex = str.toString();//force conversion
+    var result = '';
+    for (var i = 0; (i < hex.length && hex.substr(i, 2) !== '00'); i += 2)
+        result += String.fromCharCode(parseInt(hex.substr(i, 2), 16));
+    return result;
+}
+
+function ascii_to_hex(str)
+  {
+	var arr1 = [];
+	for (var n = 0, l = str.length; n < l; n ++)
+     {
+		var hex = Number(str.charCodeAt(n)).toString(16);
+		arr1.push(hex);
+	 }
+	return arr1.join('');
+   }
+
+function xor_hex(cipher1, cipher2) {
+  var result = "",
+  i = cipher1.length,
+  j = cipher2.length;
+  while (i-->0 && j-->0) {
+    result = (parseInt(cipher1.charAt(i), 16) ^ parseInt(cipher2.charAt(j), 16)).toString(16) + result;
+  }
+  return result;
+}
+
+function updateXORTable() {
+  document.getElementById("xor-ciphers").innerHTML = "";
+  var cipher1 = document.getElementById("ciphertext1").value
+  var cipher2 = document.getElementById("ciphertext2").value
+  var inputHex = xor_hex(cipher1, cipher2);
+  console.log(inputHex);
+  for(var i = 0; i < inputHex.length / 2; i++){
+    var row = document.getElementById("xor-ciphers");
+    var x = row.insertCell(i);
+    x.innerHTML = inputHex.substring(i * 2, (i * 2) + 2);
+  }
+}
+
+function updateCribTable() {
+  document.getElementById("crib-hex-table").innerHTML = "";
+  var cribword = document.getElementById("cribword").value;
+  var inputHex = ascii_to_hex(cribword);
+  for(var i = 0; i < inputHex.length / 2; i++){
+    var row = document.getElementById("crib-hex-table");
+    var x = row.insertCell(i);
+    x.innerHTML = inputHex.substring(i * 2, (i * 2) + 2);
+  }
+}
+
+
+
+var ciphersize = 50;
+var cribsize = 5;
+var screenSize = $(window).width();
+grid_size = (26);
+var sliderIndex = 0;
+var currentX = 0;
+var rightBound = ascii_to_hex(document.getElementById("cribword").value).length * 8;
+
+$(".box ")
+  .draggable({ drag: function(){
+            rightBound = ascii_to_hex(document.getElementById("cribword").value).length * 8;
+            var cribhex = ascii_to_hex(document.getElementById("cribword").value);
+            var cipherhex = "";
+            for(var i = 0; i < document.getElementById("xor-ciphers-table").getElementsByTagName("td").length; i++){
+              cipherhex = cipherhex.concat(document.getElementById("xor-ciphers-table").getElementsByTagName("td")[i].innerHTML);
+            }
+            var offset = $(this).offset();
+            var xPos = offset.left;
+            sliderIndex = (xPos- 8)/grid_size;
+            console.log("xPos = " + xPos);
+            console.log("grid_size = " + grid_size);
+            console.log('x: ' + xPos + ' index ' + sliderIndex);
+            console.log("XOR " + cipherhex.substring(((sliderIndex) * 2), ((sliderIndex) * 2) + cribhex.length));
+            console.log("AND " + cribhex);
+            xor_result = xor_hex(cipherhex.substring(((sliderIndex) * 2), ((sliderIndex) * 2) + cribhex.length), cribhex);
+            document.getElementById("crib-result").innerHTML = hex_to_ascii(xor_result);
+        }, containment: [0, 0, rightBound, 0], axis: "x", grid: [ grid_size, grid_size ] })
+	.on("mouseover", function(){
+  	$( this ).addClass("move-cursor")
+	})
+	.on("mousedown", function(){
+  	$( this )
+      .removeClass("move-cursor")
+      .addClass("grab-cursor")
+      .addClass("opac");
+
+  	$(" .text ").hide();
+
+	})
+
+	.on("mouseup", function(){
+  	$( this )
+      .removeClass("grab-cursor")
+      .removeClass("opac")
+      .addClass("move-cursor");
+	});
